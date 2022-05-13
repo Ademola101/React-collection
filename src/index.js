0f8f8da9acb68638f58d380499deab01f71cc786
Ademola <ademolaogunmokun@yahoo.com>
2022-05-13 01:10:38 +0100

    learning promise

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import ReactDOM from 'react-dom'
 import App from './App'
-
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
-  }
-]
-
+import axios from "axios"
+const promise = axios.get('127.0.0.1:3001/notes');
+console.log(promise);
+const promise2 = axios.get('127.0.0.1:3001/foobar');
+console.log(promise2);
 ReactDOM.render(
-  <App notes={notes} />,
+  <App  />,
   document.getElementById('root')
 )
