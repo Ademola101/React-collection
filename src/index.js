749ed6d98e4241ba843ec234f3ca01dcf0a9a3bb
Ademola <ademolaogunmokun@yahoo.com>
2022-05-13 01:26:45 +0100

    axios

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import ReactDOM from 'react-dom'
 import App from './App'
 import axios from "axios"
-const promise = axios.get('127.0.0.1:3001/notes');
+const promise = axios.get('http://localhost:3001/notes');
 console.log(promise);
-const promise2 = axios.get('127.0.0.1:3001/foobar');
+const promise2 = axios.get('http://localhost:3001/foobar');
 console.log(promise2);
+
+promise.then(response=> {
+  // console.log(response);
+
+  const notes = response.data
+  
+})
 ReactDOM.render(
-  <App  />,
+  <App note = {notes}  />,
   document.getElementById('root')
 )
