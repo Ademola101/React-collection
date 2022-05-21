2f704bb1a809cf4e0c4e812ec42d6de0b7dc82bf
Ademola Ogunmokun <ademolaogunmokun@yahoo.com>
2022-05-21 22:17:22 +0100

    isolate country to show hhh

--- src/App.js
+++ src/App.js
@@ -1,71 +1,54 @@
-
-import { useState, useEffect } from "react"
-import axios from "axios"
-import Input from "./components/input"
-import CountryName from "./components/CountryName"
-import OneCountry from "./components/OneCountry"
-
-
-
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Input from "./components/input";
+import CountryName from "./components/CountryName";
+import OneCountry from "./components/OneCountry";
 
 const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [userInput, setUserInput] = useState("");
 
-  const [countries, setCountries] = useState([])
-  const [userInput, setUserInput] = useState("")
-  const [countriesToShow,setCountriesToShow] = useState([])
-  const [details,setDetails] = useState(false)
-
-  
+  const [details, setDetails] = useState(false);
 
   useEffect(() => {
-    axios.get("https://restcountries.com/v3.1/all").then(response => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      setCountries(response.data);
+    });
+  }, [userInput]);
 
-    setCountries(response.data)
-    
-    }); if(userInput !== ""){
-      setCountriesToShow(countries.filter(country => 
-        country.name.common.toLowerCase().startsWith(userInput.toLocaleLowerCase()) ))
-  
-    }
-    
-  },[userInput]);
-  
-  
-  
   const textOnChange = (event) => {
-    setUserInput(event.target.value)
-    
+    setUserInput(event.target.value);
+  };
 
-    
-   };  
+  const onClick = (country) => {
+    setDetails(!details);
+  };
 
+  const countriesToShow = countries.filter((country) =>
+    country.name.common.toLowerCase().startsWith(userInput.toLocaleLowerCase())
+  );
 
-   const onClick = (country) => {
-    setDetails(!details)
-  }
-   
-    return ( <> 
-<Input value={userInput} onChange={textOnChange}/>
+  return (
+    <>
+      <Input value={userInput} onChange={textOnChange} />
 
-{countriesToShow.length  === 0 ? (<div>
-
-
-</div>) : countriesToShow.length >= 10 ? (<div>
-  Too many macthes
-</div>) : countriesToShow.length === 1 ? (<OneCountry countries = {countriesToShow}/>)
-
-
-:( <CountryName details={details} countries={countriesToShow} onClick = {onClick}/>)
-
-
-
-}
-
+      {countriesToShow.length === 0 ? (
+        <div></div>
+      ) : countriesToShow.length >= 10 ? (
+        <div>Too many macthes</div>
+      ) : countriesToShow.length === 1 ? (
+        <OneCountry countries={countriesToShow} />
+      ) : (
+        <CountryName
+          details={details}
+          countries={countriesToShow}
+          onClick={onClick}
+        />
+      )}
     </>
-    )
-  }
-   export default App 
+  );
+};
+export default App;
 
 /* const App = ({notes}) => {
 
--- src/components/CountryName.jsx
+++ src/components/CountryName.jsx
@@ -1,24 +1,25 @@
-
-const CountryName = ({details, countries,onClick}) => {  
-
-  return (<> 
-  
-  <ul>
-   {countries.map(country => { return(<li>
-      Country name:  {country.name.common} <br />
-      
-      <button onClick={onClick}>
-Show more 
-      </button>
-      {details ? (<div>
-        true
-      </div>): null}
-     </li>
-   )
-   
-     })}
-  </ul>
-  </>)}
-
+const CountryName = ({ details, countries, onClick }) => {
+  return (
+    <>
+      <ul>
+        {countries.map((country) => {
+          return (
+            <li>
+              Country name: {country.name.common} <br />
+              <button onClick={onClick}>Show more</button>
+              {details ? (
+                <div>
+                  {" "}
+                  area:
+                  {country.area}
+                </div>
+              ) : null}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
 
 export default CountryName;
